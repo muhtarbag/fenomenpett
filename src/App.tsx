ac7b26dbbc6ab9e29dd38baacb5bab6c9cd86458
muhtarbag <muhtar@fenomenbet.biz>
2024-12-03 20:56:37 +0300

    BrowserRouter to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Layout } from "@/components/layout/Layout";
 import { AppRoutes } from "@/routes/AppRoutes";
@@ -22,7 +22,7 @@ const App = () => {
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
-          <BrowserRouter>
+          <HashRouter>
             <AuthProvider>
               <Toaster />
               <Sonner />
@@ -32,7 +32,7 @@ const App = () => {
                 </Suspense>
               </Layout>
             </AuthProvider>
-          </BrowserRouter>
+          </HashRouter>
         </TooltipProvider>
       </QueryClientProvider>
     </React.StrictMode>
